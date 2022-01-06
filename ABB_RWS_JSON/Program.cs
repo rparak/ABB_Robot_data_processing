bea7078559f1fe83a93505d8f80e2f893da2d837
Roman Parak <59449320+rparak@users.noreply.github.com>
2022-01-06 12:06:14 +0000

    Update Program.cs

--- ABB_RWS_JSON/Program.cs
+++ ABB_RWS_JSON/Program.cs
@@ -23,199 +23,196 @@ Github   : https://github.com/rparak
 File Name: Program.cs
 ****************************************************************************/
 
-// ------------------------------------------------------------------------------------------------------------------------//
-// ----------------------------------------------------- LIBRARIES --------------------------------------------------------//
-// ------------------------------------------------------------------------------------------------------------------------//
-
-// -------------------- System -------------------- //
+// System Lib.
 using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
+using System.Diagnostics;
 
 namespace ABB_RWS_Data_Processing_JSON
 {
+    public static class ABB_Stream_Data
+    {
+        // IP Port Number and IP Address
+        public static string ip_address;
+        //  The target of reading the data: jointtarget / robtarget
+        public static string json_target = "";
+        // Comunication Speed (ms)
+        public static int time_step;
+        // Joint Space:
+        //  Orientation {J1 .. J6} (°)
+        public static double[] J_Orientation = new double[6];
+        // Cartesian Space:
+        //  Position {X, Y, Z} (mm)
+        public static double[] C_Position = new double[3];
+        //  Orientation {Quaternion} (-):
+        public static double[] C_Orientation = new double[4];
+    }
+
     class Program
     {
-        // -------------------- Thread -------------------- //
-        static Thread rws_read_Thread;
-        // -------------------- String -------------------- //
-        static string ip_address;
-        static string json_address;
-        static string json_address_joint, json_address_cartesian;
-        // -------------------- Double -------------------- //
-        static double[] robotBaseRotLink_ABB_c = { 0f, 0f, 0f, 0f, 0f, 0f, 0f };
-        static double[] robotBaseRotLink_ABB_j = { 0f, 0f, 0f, 0f, 0f, 0f };
-        // -------------------- Bool -------------------- //
-        static bool rws_r_while;
-        static bool communication_read_isOk;
-        // -------------------- Int -------------------- //
-        static int read_state_rws;
 
-        // ------------------------------------------------------------------------------------------------------------------------//
-        // ------------------------------------------------ MAIN FUNCTION {Cyclic} ------------------------------------------------//
-        // ------------------------------------------------------------------------------------------------------------------------//
         static void Main(string[] args)
         {
-            // ------------------------ Initialization { RWS - Robot Web Services} ------------------------//
-            // Robot IP Address
-            ip_address = "127.0.0.1";
-            // Robot - Joint address {Web}
-            json_address_joint = "/rw/rapid/tasks/T_ROB1/motion?resource=jointtarget&json=1";
-            // Robot - Cartesian address {Web}
-            json_address_cartesian = "/rw/rapid/tasks/T_ROB1/motion?resource=robtarget&json=1";
-            // Read state {0 -> Joint, 1 -> Cartesian}
-            read_state_rws = 1;
+            // Initialization {Robot Web Services ABB}
+            //  Stream Data:
+            ABB_Stream_Data.ip_address = "127.0.0.1";
+            //  The target of reading the data: jointtarget / robtarget
+            ABB_Stream_Data.json_target = "robtarget";
+            //  Communication speed (ms)
+            ABB_Stream_Data.time_step = 2;
 
-            // Set Json address
-            if (read_state_rws == 0)
-            {
-                json_address = json_address_joint;
-            }
-            else if (read_state_rws == 1)
-            {
-                json_address = json_address_cartesian;
-            }
+            // Start Stream {Universal Robots TCP/IP}
+            ABB_Stream ABB_Stream_Robot_JSON = new ABB_Stream();
+            ABB_Stream_Robot_JSON.Start();
 
-            // ------------------------ Threading Block { RWS - Read Data } ------------------------//
-            rws_r_while = true;
-            rws_read_Thread = new Thread(() => RWS_Service_read_thread_function(read_state_rws, "http://" + ip_address, json_address));
-            rws_read_Thread.IsBackground = true;
-            rws_read_Thread.Start();
+            Console.WriteLine("[INFO] Stop (y):");
+            // Stop communication
+            string stop_rs = Convert.ToString(Console.ReadLine());
 
-            // ------------------------ Main Block { Read data from the Robot (ABB) } ------------------------//
-            try
+            if (stop_rs == "y")
             {
-                // -------------------- Main Cycle {While} -------------------- //
-                while (true)
+                if (ABB_Stream_Data.json_target == "jointtarget")
                 {
-                    // -------------------- Communication {Check} -------------------- //
-                    if (communication_read_isOk == true)
-                    {
-                        // -------------------- Read State {Check} -------------------- //
-                        if (read_state_rws == 0)
-                        {
-                            // Read Joint data (1 - 6)
-                            Console.WriteLine("J1: {0} | J2: {1} | J3: {2} | J4: {3} | J5: {4} | J6: {5}",
-                                              robotBaseRotLink_ABB_j[0], robotBaseRotLink_ABB_j[1], robotBaseRotLink_ABB_j[2],
-                                              robotBaseRotLink_ABB_j[3], robotBaseRotLink_ABB_j[4], robotBaseRotLink_ABB_j[5]);
-                        }
-                        else if (read_state_rws == 1)
-                        {
-                            // Read Cartesian data (X,Y,Z, Quaternion {q1 - q4})
-                            Console.WriteLine("X: {0} | Y: {1} | Z: {2} | Q1: {3} | Q2: {4} | Q3: {5} | Q4: {6}",
-                                              robotBaseRotLink_ABB_c[0], robotBaseRotLink_ABB_c[1], robotBaseRotLink_ABB_c[2],
-                                              robotBaseRotLink_ABB_c[3], robotBaseRotLink_ABB_c[4], robotBaseRotLink_ABB_c[5],
-                                              robotBaseRotLink_ABB_c[6]);
-                        }
-                    }
-                    // Thread Sleep {100 ms}
-                    Thread.Sleep(100);
+                    Console.WriteLine("Joint Space: Orientation (radian)");
+                    Console.WriteLine("J1: {0} | J2: {1} | J3: {2} | J4: {3} | J5: {4} | J6: {5}",
+                                       ABB_Stream_Data.J_Orientation[0], ABB_Stream_Data.J_Orientation[1], ABB_Stream_Data.J_Orientation[2],
+                                       ABB_Stream_Data.J_Orientation[3], ABB_Stream_Data.J_Orientation[4], ABB_Stream_Data.J_Orientation[5]);
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                Application_Quit();
+                else if (ABB_Stream_Data.json_target == "robtarget")
+                {
+                    Console.WriteLine("Cartesian Space: Position (metres), Orientation (radian):");
+                    Console.WriteLine("X: {0} | Y: {1} | Z: {2} | Q1: {3} | Q2: {4} | Q3: {5} | Q4: {6}",
+                                       ABB_Stream_Data.C_Position[0], ABB_Stream_Data.C_Position[1], ABB_Stream_Data.C_Position[2],
+                                       ABB_Stream_Data.C_Orientation[0], ABB_Stream_Data.C_Orientation[1], ABB_Stream_Data.C_Orientation[2], ABB_Stream_Data.C_Orientation[3]);
+                }
+
+                // Destroy ABB {Stream}
+                ABB_Stream_Robot_JSON.Destroy();
+
+                // Application quit
+                Environment.Exit(0);
             }
         }
-        // ------------------------------------------------------------------------------------------------------------------------//
-        // -------------------------------------------------------- FUNCTIONS -----------------------------------------------------//
-        // ------------------------------------------------------------------------------------------------------------------------//
+    }
 
-        // -------------------- Abort Threading Blocks -------------------- //
-        static void Application_Quit()
-        {
-            try
-            {
-                // Stop - threading while (XML)
-                rws_r_while = false;
+    class ABB_Stream
+    {
+        // Initialization of Class variables
+        //  Thread
+        private Thread robot_thread = null;
+        private bool exit_thread = false;
 
-                // Abort threading block {RWS XML -> read data}
-                if (rws_read_Thread.IsAlive == true)
-                {
-                    rws_read_Thread.Abort();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-
-        // ------------------------ Threading Block { RWS - Robot Web Services (READ) } ------------------------//
-        static async void RWS_Service_read_thread_function(int read_state, string ip_adr, string json_adr)
+        async void ABB_Stream_Thread()
         {
             var handler = new HttpClientHandler { Credentials = new NetworkCredential("Default User", "robotics") };
             // disable the proxy, the controller is connected on same subnet as the PC 
             handler.Proxy = null;
             handler.UseProxy = false;
 
-            // Send a request continue when complete
-            using (HttpClient client = new HttpClient(handler))
+            try
             {
-                // while - reading {data Joint, Cartesian}
-                while (rws_r_while)
+                // Send a request continue when complete
+                using (HttpClient client = new HttpClient(handler))
                 {
-                    using (HttpResponseMessage response = await client.GetAsync(ip_adr + json_adr))
+                    // Initialization timer
+                    var t = new Stopwatch();
+
+                    while (exit_thread == false)
                     {
-                        using (HttpContent content = response.Content)
+                        // t_{0}: Timer start.
+                        t.Start();
+
+                        // Current data streaming from the source page
+                        using (HttpResponseMessage response = await client.GetAsync("http://" + ABB_Stream_Data.ip_address + "/rw/rapid/tasks/T_ROB1/motion?resource=" + ABB_Stream_Data.json_target + "&json=1"))
                         {
-                            try
+                            using (HttpContent content = response.Content)
                             {
-                                // set variable {communation is ok}
-                                communication_read_isOk = true;
-                                // Check that response was successful or throw exception
-                                response.EnsureSuccessStatusCode();
-                                // Get HTTP response from completed task.
-                                string result = await content.ReadAsStringAsync();
-                                // Deserialize the returned json string
-                                dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
-
-                                // Display controller name, version and version name
-                                var service = obj._embedded._state[0];
-
-                                if (read_state == 0)
+                                try
                                 {
-                                    // Joint {1 .. 6} -> Read RWS JSON
-                                    robotBaseRotLink_ABB_j[0] = Math.Round((double)service.j1, 2);
-                                    robotBaseRotLink_ABB_j[1] = Math.Round((double)service.j2, 2);
-                                    robotBaseRotLink_ABB_j[2] = Math.Round((double)service.j3, 2);
-                                    robotBaseRotLink_ABB_j[3] = Math.Round((double)service.j4, 2);
-                                    robotBaseRotLink_ABB_j[4] = Math.Round((double)service.j5, 2);
-                                    robotBaseRotLink_ABB_j[5] = Math.Round((double)service.j6, 2);
-                                }
-                                else if (read_state == 1)
-                                {
-                                    // TCP {X, Y, Z} -> Read RWS JSON
-                                    robotBaseRotLink_ABB_c[0] = Math.Round((double)service.x, 2);
-                                    robotBaseRotLink_ABB_c[1] = Math.Round((double)service.y, 2);
-                                    robotBaseRotLink_ABB_c[2] = Math.Round((double)service.z, 2);
-                                    // Quaternion {q1 .. q4} -> Read RWS JSON
-                                    robotBaseRotLink_ABB_c[3] = Math.Round((double)service.q1, 6);
-                                    robotBaseRotLink_ABB_c[4] = Math.Round((double)service.q2, 6);
-                                    robotBaseRotLink_ABB_c[5] = Math.Round((double)service.q3, 6);
-                                    robotBaseRotLink_ABB_c[6] = Math.Round((double)service.q4, 6);
-                                }
+                                    // Check that response was successful or throw exception
+                                    response.EnsureSuccessStatusCode();
+                                    // Get HTTP response from completed task.
+                                    string result = await content.ReadAsStringAsync();
+                                    // Deserialize the returned json string
+                                    dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
 
-                                // Thread Sleep {200 ms}
-                                Thread.Sleep(200);
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine(e.Message);
-                            }
-                            finally
-                            {
-                                content.Dispose();
+                                    // Display controller name, version and version name
+                                    var service = obj._embedded._state[0];
+
+                                    if (ABB_Stream_Data.json_target == "jointtarget")
+                                    {
+                                        // Joint {1 .. 6} -> Read RWS JSON
+                                        ABB_Stream_Data.J_Orientation[0] = (double)service.j1;
+                                        ABB_Stream_Data.J_Orientation[1] = (double)service.j2;
+                                        ABB_Stream_Data.J_Orientation[2] = (double)service.j3;
+                                        ABB_Stream_Data.J_Orientation[3] = (double)service.j4;
+                                        ABB_Stream_Data.J_Orientation[4] = (double)service.j5;
+                                        ABB_Stream_Data.J_Orientation[5] = (double)service.j6;
+                                    }
+                                    else if (ABB_Stream_Data.json_target == "robtarget")
+                                    {
+                                        // TCP {X, Y, Z} -> Read RWS JSON
+                                        ABB_Stream_Data.C_Position[0] = (double)service.x;
+                                        ABB_Stream_Data.C_Position[1] = (double)service.y;
+                                        ABB_Stream_Data.C_Position[2] = (double)service.z;
+                                        // Quaternion {q1 .. q4} -> Read RWS JSON
+                                        ABB_Stream_Data.C_Orientation[0] = (double)service.q1;
+                                        ABB_Stream_Data.C_Orientation[1] = (double)service.q2;
+                                        ABB_Stream_Data.C_Orientation[2] = (double)service.q3;
+                                        ABB_Stream_Data.C_Orientation[3] = (double)service.q4;
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                }
                             }
                         }
+
+                        // t_{1}: Timer stop.
+                        t.Stop();
+
+                        // Recalculate the time: t = t_{1} - t_{0} -> Elapsed Time in milliseconds
+                        if (t.ElapsedMilliseconds < ABB_Stream_Data.time_step)
+                        {
+                            Thread.Sleep(ABB_Stream_Data.time_step - (int)t.ElapsedMilliseconds);
+                        }
+
+                        // Reset (Restart) timer.
+                        t.Restart();
                     }
                 }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("Communication Problem: {0}", e);
+            }
+        }
+
+        public void Start()
+        {
+            exit_thread = false;
+            // Start a thread to stream ABB Robot
+            robot_thread = new Thread(new ThreadStart(ABB_Stream_Thread));
+            robot_thread.IsBackground = true;
+            robot_thread.Start();
+        }
+        public void Stop()
+        {
+            exit_thread = true;
+            // Start a thread
+            if (robot_thread.IsAlive == true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+        public void Destroy()
+        {
+            // Stop a thread (Robot Web Services communication)
+            Stop();
+            Thread.Sleep(100);
         }
     }
 }
